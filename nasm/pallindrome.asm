a477e8ce456f10e40133617931ba61d55c2deda3
vidya23a <127966996+vidya23a@users.noreply.github.com>
2024-11-10 10:20:49 +0530

    pallindrome.asm

--- nasm/pallindrome.asm
+++ nasm/pallindrome.asm
@@ -1,20 +1,19 @@
 section .data
-    msg1 db "String",0xa
-    msg2 db "Pallindrome",0xa
-    msg3 db "Not pallindrome",0xa
+    msg1 db "enter:"
+    msg2 db "pallindrome"
+    msg3 db "no"
     
 section .bss
     str1 resb 100
-    str2 resb 100
     
 section .text
     global _start
-
+    
 _start:
     mov eax,4
     mov ebx,1
     mov ecx,msg1
-    mov edx,7
+    mov edx,6
     int 0x80
     
     mov eax,3
@@ -23,48 +22,35 @@ _start:
     mov edx,100
     int 0x80
     mov byte[str1+eax-1],0
-    
     mov ecx,eax
     dec ecx
-    mov esi,str1
-    mov edi,str2
-    add esi,ecx
-    dec esi
     
+    mov esi,str1
+    mov edi,str1
+    add edi,ecx
+    dec edi
     loop1:mov al,[esi]
-    mov [edi],al
-    inc edi
-    dec esi
-    loop loop1
-    
-    mov byte[edi],0
-    mov esi,str1
-    mov edi,str2
-    
-    loop2:mov al,[esi]
     mov bl,[edi]
     cmp al,bl
-    jne not_equal
+    jne no_pal
     inc esi
-    inc edi
-    test al,al
-    jz end
-    test bl,bl
-    jz not_equal
-    jmp loop2
+    dec edi
+    test al,al      ;cmp esi,edi
+    jz done         ;jge done
+    jmp loop1
     
-    not_equal:mov eax,4
-    mov ebx,1
-    mov ecx,msg3
-    mov edx,16
-    int 0x80
-    jmp finish
-    
-    end:mov eax,4
+    done:mov eax,4
     mov ebx,1
     mov ecx,msg2
-    mov edx,12
+    mov edx,11
+    int 0x80
+    jmp end
+    
+    no_pal:mov eax,4
+    mov ebx,1
+    mov ecx,msg3
+    mov edx,2
     int 0x80
     
-    finish:mov eax,1
+    end:mov eax,1
     int 0x80
